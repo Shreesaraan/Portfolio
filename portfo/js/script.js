18e79ca8c85149361419785624c51fdfb4aa0fde
Shree Dev <Shreedev2k3@gmail.com>
2023-06-21 23:00:39 +0530

    Add files via upload

---
+++ portfo/js/script.js
@@ -0,0 +1,8 @@
+script.js
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
